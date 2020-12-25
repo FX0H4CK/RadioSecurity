c42d3a274484f3ef36c34c7d754f90cf6bea2091
FX0H4CK <franzi@f-obermayer.de>
2020-12-25 18:52:10 +0000

    Webinterface Styling

--- Web/Interface/web.py
+++ Web/Interface/web.py
@@ -12,10 +12,104 @@ app = Flask(__name__)
 @app.route("/home")
 @app.route("/")
 def home():
-        return render_template("index.html")
+        try:
+            conn = mariadb.connect(
+                    user="reader",
+                    password="PW4readonly",
+                    host="127.0.0.1",
+                    port=3306,
+                    database="radio_mon"
+            )
+        except mariadb.Error as e:
+            print(f"[x] Error while Connecting: {e}")
+        cursor = conn.cursor()
+        try:
+            cursor.execute("SELECT * FROM ssids where status=1;")
+            unknown = cursor.fetchall()
+            cursor.execute("SELECT * FROM ssids;")
+            allssids = cursor.fetchall()
+            percent=len(unknown)/len(allssids)*100;
+            percent=int(percent)
+            if percent<70:
+                color='orange'
+            else:
+                color='green'
+           
+            cursor.execute("SELECT * FROM ssids where privacy LIKE '%WPA2%';")
+            encrypted = cursor.fetchall()
+            percent_enc=len(encrypted)/len(allssids)*100;
+            percent_enc=int(percent_enc)
+            if percent_enc>70:
+                color_enc='green'
+            else:
+                color_enc='orange'
+
+        except mariadb.Error as e:
+            print(f"[x] Error while Reading Data: {e}")
+        return render_template("index.html", percent=percent, color=color, percent_enc=percent_enc,color_enc=color_enc)
+
 
 @app.route("/wifi", methods=["POST", "GET"])
 def wifi():
+    if request.method == "POST":
+        accept = request.form["accept"]
+        mac = accept.split(',',1)[0].replace(' ','')
+        ssid = accept.split(',',1)[1].replace(' ','')
+        print(mac)
+        print(ssid)
+        try:
+            conn = mariadb.connect(
+                    user="writer",
+                    password="PW4Writer!",
+                    host="127.0.0.1",
+                    port=3306,
+                    database="radio_mon"
+            )
+        except mariadb.Error as e:
+            print(f"[x] Error while connecting: {e}")
+        cursor = conn.cursor()
+        try:
+            cursor.execute("UPDATE ssids SET status=1 WHERE bssid='"+mac+"' AND essid=' "+ssid+"';")
+            print("UPDATE ssids SET status=1 WHERE bssid='"+mac+"' AND essid=' "+ssid+"';")
+            conn.commit()
+            conn.close()
+        except mariadb.Error as e:
+            print(f"[x] Error while executing update: {e}")
+
+        return redirect(url_for("wifi"))
+    else:
+        for i in range(0,15):
+            exec("global SSID%d; global ENC%d; global MAC%d; global ls%d; global vendor%d; SSID%d='';ENC%d=''; MAC%d='';ls%d='';vendor%d='';"% (i+1, i+1, i+1, i+1,i+1,i+1,i+1,i+1,i+1,i+1));
+        try:
+            conn = mariadb.connect(
+                    user="reader",
+                    password="PW4readonly",
+                    host="127.0.0.1",
+                    port=3306,
+                    database="radio_mon"
+            )
+        except mariadb.Error as e:
+            print(f"[x] Error while Connecting: {e}")
+        cursor = conn.cursor()
+        try:
+            cursor.execute("SELECT * FROM ssids;")
+            ssids = cursor.fetchall()
+        except mariadb.Error as e:
+            print(f"[x] Error while Reading Data: {e}")
+        
+        for i in range(1,len(ssids)):
+                exec("""global SSID%d; SSID%d = ssids[%d][13].replace('" "', 'Versteckt').replace('"', '');""" % (i,i,i));
+                exec("""global ENC%d; ENC%d = ssids[%d][5].replace('"', '');""" % (i,i,i)); 
+                exec("""global MAC%d; MAC%d = ssids[%d][0].replace('"', '');""" % (i,i,i));
+                exec("""global ls%d; ls%d = ssids[%d][2];""" % (i,i,i));
+                exec("""global vendor%d; vendor%d = ssids[%d][14];"""% (i,i,i));
+        conn.close()
+
+        return render_template("wifi.html", SSID1=SSID1, SSID2=SSID2, SSID3=SSID3, SSID4=SSID4, SSID5=SSID5, SSID6=SSID6, SSID7=SSID7, SSID8=SSID8, SSID9=SSID9, SSID10=SSID10, SSID11=SSID11, SSID12=SSID12, SSID13=SSID13, SSID14=SSID14, SSID15=SSID15, Encryption1=ENC1, Encryption2=ENC2, Encryption3=ENC3, Encryption4=ENC4, Encryption5=ENC5, Encryption6=ENC6, Encryption7=ENC7, Encryption8=ENC8, Encryption9=ENC9, Encryption10=ENC10, Encryption11=ENC11, Encryption12=ENC12, Encryption13=ENC13, Encryption14=ENC14, Encryption15=ENC15, MAC1=MAC1, MAC2=MAC2, MAC3=MAC3, MAC4=MAC4, MAC5=MAC5, MAC6=MAC6, MAC7=MAC7,MAC8=MAC8,MAC9=MAC9,MAC10=MAC10,MAC11=MAC11,MAC12=MAC12,MAC13=MAC13,MAC14=MAC14,MAC15=MAC15,ls1=ls1,ls2=ls2,ls3=ls3,ls4=ls4,ls5=ls5,ls6=ls6,ls7=ls7,ls8=ls8,ls9=ls9,ls10=ls10,ls11=ls11,ls12=ls12,ls13=ls13,ls14=ls14,ls15=ls15, vendor1=vendor1, vendor2=vendor2, vendor3=vendor2,vendor4=vendor4,vendor5=vendor5,vendor6=vendor6,vendor7=vendor7,vendor8=vendor8,vendor9=vendor9,vendor10=vendor10,vendor11=vendor11,vendor12=vendor12,vendor13=vendor13,vendor14=vendor14,vendor15=vendor15)
+
+
+@app.route("/wifi/unknown", methods=["POST", "GET"])
+def unknownwifi():
     if request.method == "POST":
         accept = request.form["accept"]
         mac = accept.split(',',1)[0].replace(' ','')
@@ -70,7 +164,7 @@ def wifi():
                 exec("""global vendor%d; vendor%d = ssids[%d][14];"""% (i,i,i));
         conn.close()
 
-        return render_template("wifi.html", SSID1=SSID1, SSID2=SSID2, SSID3=SSID3, SSID4=SSID4, SSID5=SSID5, SSID6=SSID6, SSID7=SSID7, SSID8=SSID8, SSID9=SSID9, SSID10=SSID10, SSID11=SSID11, SSID12=SSID12, SSID13=SSID13, SSID14=SSID14, SSID15=SSID15, Encryption1=ENC1, Encryption2=ENC2, Encryption3=ENC3, Encryption4=ENC4, Encryption5=ENC5, Encryption6=ENC6, Encryption7=ENC7, Encryption8=ENC8, Encryption9=ENC9, Encryption10=ENC10, Encryption11=ENC11, Encryption12=ENC12, Encryption13=ENC13, Encryption14=ENC14, Encryption15=ENC15, MAC1=MAC1, MAC2=MAC2, MAC3=MAC3, MAC4=MAC4, MAC5=MAC5, MAC6=MAC6, MAC7=MAC7,MAC8=MAC8,MAC9=MAC9,MAC10=MAC10,MAC11=MAC11,MAC12=MAC12,MAC13=MAC13,MAC14=MAC14,MAC15=MAC15,ls1=ls1,ls2=ls2,ls3=ls3,ls4=ls4,ls5=ls5,ls6=ls6,ls7=ls7,ls8=ls8,ls9=ls9,ls10=ls10,ls11=ls11,ls12=ls12,ls13=ls13,ls14=ls14,ls15=ls15, vendor1=vendor1, vendor2=vendor2, vendor3=vendor2,vendor4=vendor4,vendor5=vendor5,vendor6=vendor6,vendor7=vendor7,vendor8=vendor8,vendor9=vendor9,vendor10=vendor10,vendor11=vendor11,vendor12=vendor12,vendor13=vendor13,vendor14=vendor14,vendor15=vendor15)
+        return render_template("wifi.html",button='Bekannt', SSID1=SSID1, SSID2=SSID2, SSID3=SSID3, SSID4=SSID4, SSID5=SSID5, SSID6=SSID6, SSID7=SSID7, SSID8=SSID8, SSID9=SSID9, SSID10=SSID10, SSID11=SSID11, SSID12=SSID12, SSID13=SSID13, SSID14=SSID14, SSID15=SSID15, Encryption1=ENC1, Encryption2=ENC2, Encryption3=ENC3, Encryption4=ENC4, Encryption5=ENC5, Encryption6=ENC6, Encryption7=ENC7, Encryption8=ENC8, Encryption9=ENC9, Encryption10=ENC10, Encryption11=ENC11, Encryption12=ENC12, Encryption13=ENC13, Encryption14=ENC14, Encryption15=ENC15, MAC1=MAC1, MAC2=MAC2, MAC3=MAC3, MAC4=MAC4, MAC5=MAC5, MAC6=MAC6, MAC7=MAC7,MAC8=MAC8,MAC9=MAC9,MAC10=MAC10,MAC11=MAC11,MAC12=MAC12,MAC13=MAC13,MAC14=MAC14,MAC15=MAC15,ls1=ls1,ls2=ls2,ls3=ls3,ls4=ls4,ls5=ls5,ls6=ls6,ls7=ls7,ls8=ls8,ls9=ls9,ls10=ls10,ls11=ls11,ls12=ls12,ls13=ls13,ls14=ls14,ls15=ls15, vendor1=vendor1, vendor2=vendor2, vendor3=vendor2,vendor4=vendor4,vendor5=vendor5,vendor6=vendor6,vendor7=vendor7,vendor8=vendor8,vendor9=vendor9,vendor10=vendor10,vendor11=vendor11,vendor12=vendor12,vendor13=vendor13,vendor14=vendor14,vendor15=vendor15)
 
 @app.route("/wifi/known", methods=["POST", "GET"])
 def knownwifi():
@@ -127,7 +221,7 @@ def knownwifi():
                 exec("""global vendor%d; vendor%d = ssids[%d][14];"""% (i,i,i));
         conn.close()
 
-        return render_template("wifi.html", SSID1=SSID1, SSID2=SSID2, SSID3=SSID3, SSID4=SSID4, SSID5=SSID5, SSID6=SSID6, SSID7=SSID7, SSID8=SSID8, SSID9=SSID9, SSID10=SSID10, SSID11=SSID11, SSID12=SSID12, SSID13=SSID13, SSID14=SSID14, SSID15=SSID15, Encryption1=ENC1, Encryption2=ENC2, Encryption3=ENC3, Encryption4=ENC4, Encryption5=ENC5, Encryption6=ENC6, Encryption7=ENC7, Encryption8=ENC8, Encryption9=ENC9, Encryption10=ENC10, Encryption11=ENC11, Encryption12=ENC12, Encryption13=ENC13, Encryption14=ENC14, Encryption15=ENC15, MAC1=MAC1, MAC2=MAC2, MAC3=MAC3, MAC4=MAC4, MAC5=MAC5, MAC6=MAC6, MAC7=MAC7,MAC8=MAC8,MAC9=MAC9,MAC10=MAC10,MAC11=MAC11,MAC12=MAC12,MAC13=MAC13,MAC14=MAC14,MAC15=MAC15,ls1=ls1,ls2=ls2,ls3=ls3,ls4=ls4,ls5=ls5,ls6=ls6,ls7=ls7,ls8=ls8,ls9=ls9,ls10=ls10,ls11=ls11,ls12=ls12,ls13=ls13,ls14=ls14,ls15=ls15, vendor1=vendor1, vendor2=vendor2, vendor3=vendor2,vendor4=vendor4,vendor5=vendor5,vendor6=vendor6,vendor7=vendor7,vendor8=vendor8,vendor9=vendor9,vendor10=vendor10,vendor11=vendor11,vendor12=vendor12,vendor13=vendor13,vendor14=vendor14,vendor15=vendor15)
+        return render_template("wifi.html",button="unbekannt", SSID1=SSID1, SSID2=SSID2, SSID3=SSID3, SSID4=SSID4, SSID5=SSID5, SSID6=SSID6, SSID7=SSID7, SSID8=SSID8, SSID9=SSID9, SSID10=SSID10, SSID11=SSID11, SSID12=SSID12, SSID13=SSID13, SSID14=SSID14, SSID15=SSID15, Encryption1=ENC1, Encryption2=ENC2, Encryption3=ENC3, Encryption4=ENC4, Encryption5=ENC5, Encryption6=ENC6, Encryption7=ENC7, Encryption8=ENC8, Encryption9=ENC9, Encryption10=ENC10, Encryption11=ENC11, Encryption12=ENC12, Encryption13=ENC13, Encryption14=ENC14, Encryption15=ENC15, MAC1=MAC1, MAC2=MAC2, MAC3=MAC3, MAC4=MAC4, MAC5=MAC5, MAC6=MAC6, MAC7=MAC7,MAC8=MAC8,MAC9=MAC9,MAC10=MAC10,MAC11=MAC11,MAC12=MAC12,MAC13=MAC13,MAC14=MAC14,MAC15=MAC15,ls1=ls1,ls2=ls2,ls3=ls3,ls4=ls4,ls5=ls5,ls6=ls6,ls7=ls7,ls8=ls8,ls9=ls9,ls10=ls10,ls11=ls11,ls12=ls12,ls13=ls13,ls14=ls14,ls15=ls15, vendor1=vendor1, vendor2=vendor2, vendor3=vendor2,vendor4=vendor4,vendor5=vendor5,vendor6=vendor6,vendor7=vendor7,vendor8=vendor8,vendor9=vendor9,vendor10=vendor10,vendor11=vendor11,vendor12=vendor12,vendor13=vendor13,vendor14=vendor14,vendor15=vendor15)
 
 @app.route("/ble")
 def ble():
